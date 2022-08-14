5a2e6455fd29bf399609afd4b1977fdf68af4c4c
Mohamed Ali Hafi <mohamed.ali.hafi.contact@gmail.com>
2022-08-14 11:06:15 +0100

    Link navbar items with pages

--- src/App.js
+++ src/App.js
@@ -14,8 +14,7 @@ function App() {
         <Route path='/projects' component={Projects} />
         <Route path='/skills' component={Skills} />
         <Route path='/contact' component={Contact} />
-        <h1>Hello</h1>
-        <button className="btn btn-success">OK</button>
+        
       </BrowserRouter>
     </div>
   );
