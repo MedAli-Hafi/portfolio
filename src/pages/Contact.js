9ebb6d6aa5e95c0d96b3244b09b5b63388d616ea
Mohamed Ali Hafi <mohamed.ali.hafi.contact@gmail.com>
2022-08-20 10:03:29 +0100

    Add content to the Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
 function Contact() {
+    const contactInformations = {
+        email: 'mohamed.ali.hafi.contact@gmail.com',
+        phoneNumber: '+216 24 100 995',
+        address: {
+            city: 'Sfax',
+            country: 'Tunisia',
+            street: 'AVENUE MAJIDA BOULILA',
+            postalCode: 3027
+        }
+    }
     return (
-        <div>Contact</div>
+        <div>
+            <p> <i class="bi bi-envelope"></i> {contactInformations.email}</p>
+            <p> <i class="bi bi-telephone"></i> {contactInformations.phoneNumber}</p>
+            <p> <i class="bi bi-geo-alt"></i> {contactInformations.address.street} ; {contactInformations.address.postalCode} {contactInformations.address.city} ; {contactInformations.address.country}</p>
+            <p></p>
+        </div>
     )
 }
 
